d5af6c29e96a71c27e3638b766401ba89d022fe7
DarkNikito <nikitos.barbados@gmail.com>
2023-12-21 08:47:07 +0100

    almost done

--- src/main/java/com/example/demo/Games.java
+++ src/main/java/com/example/demo/Games.java
@@ -2,14 +2,18 @@ package com.example.demo;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
+@Setter
+@Getter
 @Entity
 public class Games implements Serializable {
     @Id
 
-    private int Indeks;
+
     private String Title;
     private String IMG_small;
     private String IMG_big;
@@ -24,114 +28,4 @@ public class Games implements Serializable {
     private String Platform;
     private int ElementId;
 
-    public int getIndeks() {
-        return Indeks;
-    }
-
-    public void setIndeks(int Indeks) {
-        this.Indeks = Indeks;
-    }
-
-    public String getTitle() {
-        return Title;
-    }
-
-    public void setTitle(String Title) {
-        this.Title = Title;
-    }
-
-    public String getIMG_small() {
-        return IMG_small;
-    }
-
-    public void setIMG_small(String IMG) {
-        this.IMG_small = IMG;
-    }
-
-    public String getIMG_big() {
-        return IMG_big;
-    }
-
-    public void setIMG_big(String IMG) {
-        this.IMG_big = IMG;
-    }
-
-    public String getDeveloper() {
-        return Developer;
-    }
-
-    public void setDeveloper(String Developer) {
-        this.Developer = Developer;
-    }
-
-    public String getPublisher() {
-        return Publisher;
-    }
-
-    public void setPublisher(String Publisher) {
-        this.Publisher = Publisher;
-    }
-
-    public String getLink() {
-        return Link;
-    }
-
-    public void setLink(String Link) {
-        this.Link = Link;
-    }
-
-    public double getPrice() {
-        return Price;
-    }
-
-    public void setPrice(double Price) {
-        this.Price = Price;
-    }
-
-    public int getReviews() {
-        return Reviews;
-    }
-
-    public void setReviews(int Reviews) {
-        this.Reviews = Reviews;
-    }
-
-    public int getPositive() {
-        return Positive;
-    }
-
-    public void setPositive(int Positive) {
-        this.Positive = Positive;
-    }
-
-    public int getSale() {
-        return Sale;
-    }
-
-    public void setSale(int Sale) {
-        this.Sale = Sale;
-    }
-
-    public String getDate() {
-        return Date;
-    }
-
-    public void setDate(String Date) {
-        this.Date = Date;
-    }
-
-    public String getPlatform() {
-        return Platform;
-    }
-
-    public void setPlatform(String Platform) {
-        this.Platform = Platform;
-    }
-
-    public int getElementId() {
-        return ElementId;
-    }
-    public void setElementId(int ElementId) {
-        this.ElementId = ElementId;
-    }
 }
--- src/main/java/com/example/demo/PageController.java
+++ src/main/java/com/example/demo/PageController.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
 import java.util.*;
 
 import static java.lang.Double.*;
@@ -19,39 +20,56 @@ import static java.lang.Double.*;
 @Slf4j
 public class PageController {
 
-    String[] columns = {"","Title", "", "Price", "Reviews", "Positive", "Platform"};
     Filters prev_params;
     Games[] games;
-    Games randomgame;
+    Games randomGame;
 
 
     @RequestMapping("/random")
-    public String random(Model model) throws SQLException {
-        gamelist(new Filters(), true);                             //random game from sql
-        model.addAttribute("hd", columns);                    //push attributes
-        model.addAttribute("game", randomgame);
+    public String random(Model model) {
+        try {
+            gamelist(new Filters(), true);                         //search games in sql
+        } catch (Exception e) {
+            model.addAttribute("error", "Seems like we are updating our DB, sorry, try again later");
+            log.error("SQL is not working!");
+            return "error";
+        }
+        int a = randomGame.getReviews();
+        int b = randomGame.getPositive();
+        DecimalFormat df = new DecimalFormat("#.##");
+        model.addAttribute("game", randomGame);               //push attribute
+        model.addAttribute("revs", new int[]{a / 100 * b, a / 100 * (100 - b)});
+        model.addAttribute("oldcost", df.format(randomGame.getPrice()/(100-randomGame.getSale())*100));
         return "random";
     }
 
 
     @RequestMapping("/search")
-    public String search(@RequestParam Map<String, String> queryParameters, Model model) throws SQLException {
+    public String search(@RequestParam Map<String, String> queryParameters, Model model) {
         Filters params = paramsAsNeeded(queryParameters);                  //param initializing
-        if (!params.equals(prev_params)){
+        if (! params.equals(prev_params)) {
             prev_params = params;
-            gamelist(params, false);                               //search games in sql
+            try {
+                gamelist(params, false);                           //search games in sql
+            } catch (Exception e) {
+                model.addAttribute("error", "Seems like we are updating our DB, sorry, try again later");
+                log.error("SQL is not working!");
+                return "error";
+            }
         }
         int page_length = 25;
         int max_pages = games.length / page_length;
-        if (params.getPage() < 0 || params.getPage() > max_pages){        //number of page to be returned
-            params.setPage(0);}
-        int index = params.getPage()*page_length;
+        if (params.getPage() < 0 || params.getPage() > max_pages) {        //number of page to be returned
+            params.setPage(0);
+        }
+        int index = params.getPage() * page_length;
         Games[] page = new Games[Integer.min(games.length - index, page_length)];
         System.arraycopy(games, index, page, 0, Integer.min(games.length - index, page_length));
-        for (int i = 0; i < Integer.min(games.length - index, page_length); i++) {page[i].setElementId(i);}
-        log.info("Search by params:"+params);
+        for (int i = 0; i < Integer.min(games.length - index, page_length); i++) {
+            page[i].setElementId(i);
+        }
+        log.info("Search by params:" + params);
         model.addAttribute("games", page);                     //push attributes
-        model.addAttribute("hd", columns);
         model.addAttribute("page", params.getPage() + 1);
         model.addAttribute("maxPage", max_pages + 1);
         model.addAttribute("gamesAmount", games.length);
@@ -73,18 +91,20 @@ public class PageController {
         String[] Row = new String[columns_amount];
         ResultSet rs;
         ResultSet amount;
-        if (!random) {
-            rs = conn.createStatement().executeQuery("SELECT * "+question);
-            amount = conn.createStatement().executeQuery("SELECT COUNT(*) "+question);
+        if (! random) {
+            rs = conn.createStatement().executeQuery("SELECT * " + question);
+            amount = conn.createStatement().executeQuery("SELECT COUNT(*) " + question);
         } else {
             Random rand = new Random();
             amount = conn.createStatement().executeQuery("SELECT COUNT(*) FROM steam");
             amount.next();
-            int num = rand.nextInt(Math.toIntExact(amount.getInt(1)))+1;
+            int num = rand.nextInt(Math.toIntExact(amount.getInt(1))) + 1;
             rs = conn.createStatement().executeQuery("SELECT * FROM steam WHERE Indeks LIKE " + num);
             rs.next();
-            for (int i = 1; i <= columns_amount; i++) {Row[i - 1] = rs.getString(i);}
-            randomgame = arrayToGames(Row);
+            for (int i = 1; i <= columns_amount; i++) {
+                Row[i - 1] = rs.getString(i);
+            }
+            randomGame = arrayToGames(Row);
             log.info("Successfully returned random game");
             return;
         }
@@ -92,9 +112,11 @@ public class PageController {
         games = new Games[Math.min(amount.getInt(1), 1000)];
         int n = 0;
         while (rs.next()) {
-            for (int i = 1; i <= columns_amount; i++) {Row[i - 1] = rs.getString(i);}
-            games[n++] = arrayToGames(Row);
+            for (int i = 1; i <= columns_amount; i++) {
+                Row[i - 1] = rs.getString(i);
             }
+            games[n++] = arrayToGames(Row);
+        }
         log.info("Successfully returned games");
     }
 
@@ -108,45 +130,79 @@ public class PageController {
         game.setDeveloper(line[n++]);
         game.setPublisher(line[n++]);
         game.setLink(line[n++]);
-        game.setPrice(parseDouble(line[n++])/100d);
+        game.setPrice(parseDouble(line[n++]) / 100d);
         game.setReviews(Integer.parseInt(line[n++]));
         game.setPositive(Integer.parseInt(line[n++]));
         game.setSale(Integer.parseInt(line[n++]));
         game.setDate(line[n++]);
-        game.setPlatform("images/"+line[n++]+"_logo.png");
+        game.setPlatform("images/" + line[n] + "_logo.png");
         return game;
     }
 
-    //a little fix for all filters + handling some errors user can make TODO append(change/optimise ?)
-    private Filters paramsAsNeeded(Map<String, String> params){
+    //a little fix for all filters + handling some errors user can make
+    private Filters paramsAsNeeded(Map<String, String> params) {
         Filters new_params = new Filters();
-        if (Objects.equals(params.get("Title"), "undefined") || params.get("Title") == null){new_params.setByTitle("");}
-        else{ new_params.setByTitle(params.get("Title"));}
-        if (Objects.equals(params.get("Developer"), "undefined") || params.get("Developer") == null){new_params.setByDeveloper("");}
-        else{ new_params.setByDeveloper(params.get("Developer"));}
-        if (Objects.equals(params.get("Publisher"), "undefined") || params.get("Publisher") == null){new_params.setByPublisher("");}
-        else{ new_params.setByPublisher(params.get("Publisher"));}
-        if (!Objects.equals(params.get("PriceMin"), "") & !Objects.equals(params.get("PriceMax"), "") & params.get("PriceMin") != null & params.get("PriceMax") != null
-                /*& !Objects.equals(params.get("PriceMin"), "undefined")
-                & !Objects.equals(params.get("PriceMax"), "undefined")*/) {
-            if (parseDouble(params.get("PriceMax")) >= parseDouble(params.get("PriceMin"))) {
-                new_params.setByPriceMin(parseDouble(params.get("PriceMin")));
-                new_params.setByPriceMax(parseDouble(params.get("PriceMax")));
-            } else {
-                log.warn("filter PriceMin > PriceMax, fixed");
-                new_params.setByPriceMin(parseDouble(params.get("PriceMax")));
-                new_params.setByPriceMax(parseDouble(params.get("PriceMin")));
-            }
+
+        if (params.get("Title") == null) {
+            new_params.setByTitle("");
         } else {
+            new_params.setByTitle(params.get("Title"));
+        }
+
+        if (params.get("Developer") == null) {
+            new_params.setByDeveloper("");
+        } else {
+            new_params.setByDeveloper(params.get("Developer"));
+        }
+
+        if (params.get("Publisher") == null) {
+            new_params.setByPublisher("");
+        } else {
+            new_params.setByPublisher(params.get("Publisher"));
+        }
+
+        try {
+            if (! Objects.equals(params.get("PriceMin"), "") & ! Objects.equals(params.get("PriceMax"), "")
+                    & params.get("PriceMin") != null & params.get("PriceMax") != null) {
+                if (parseDouble(params.get("PriceMax")) >= parseDouble(params.get("PriceMin"))) {
+                    new_params.setByPriceMin(parseDouble(params.get("PriceMin")));
+                    new_params.setByPriceMax(parseDouble(params.get("PriceMax")));
+                } else {
+                    log.warn("filter PriceMin > PriceMax, fixed");
+                    new_params.setByPriceMin(parseDouble(params.get("PriceMax")));
+                    new_params.setByPriceMax(parseDouble(params.get("PriceMin")));
+                }
+            } else {
+                new_params.setByPriceMin(0);
+                new_params.setByPriceMax(1000);
+            }
+        } catch (Exception e) {
+            log.warn("Price: " + e);
             new_params.setByPriceMin(0);
             new_params.setByPriceMax(1000);
         }
-        if (Objects.equals(params.get("Positive"), "undefined") || Objects.equals(params.get("Positive"), "")
-                || params.get("Positive") == null) {new_params.setByPositive(0);}
-        else{new_params.setByPositive(Integer.parseInt(params.get("Positive")));}
-        if (Objects.equals(params.get("Page"), "undefined") || Objects.equals(params.get("Page"), "")
-                || params.get("Page") == null) {new_params.setPage(0);}
-        else{new_params.setPage(Integer.parseInt(params.get("Page"))-1);}
+
+        try {
+            if (! Objects.equals(params.get("Positive"), "") || params.get("Positive") != null) {
+                new_params.setByPositive(Integer.parseInt(params.get("Positive")));
+            } else {
+                new_params.setByPositive(0);
+            }
+        } catch (Exception e) {
+            log.warn("Positive: " + e);
+            new_params.setByPositive(0);
+        }
+        try {
+            if (Objects.equals(params.get("Page"), "") || params.get("Page") == null) {
+                new_params.setPage(0);
+            } else {
+                new_params.setPage(Integer.parseInt(params.get("Page")) - 1);
+            }
+        } catch (Exception e) {
+            log.warn("Page: " + e);
+            new_params.setPage(0);
+        }
+
         return new_params;
     }
 }
